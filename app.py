e01ea76bd4e77c1ea1c08fe22f6535fcb65c349d
Vinícius <vinicius.silva120@gmail.com>
2024-08-02 15:59:41 -0300

    feat: Add POST method
    It adds the POST method, allowing to insert a meal in database.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 from database import db
+from models.meal import Meal
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key'
@@ -8,9 +9,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
 
 
-@app.route('/hello-world', methods=['GET'])
-def hello_world():
-    return 'Hello, world!'
+@app.route('/meal', methods=['POST'])
+def create_meal():
+    """
+    This function creates a new meal.
+    Returns:
+        Response: Message with the created meal.
+    """
+
+    data = request.get_json()
+    name = data.get('name')
+    description = data.get('description')
+    datetime = data.get('date_hour')
+    is_in_diet = data.get('is_in_diet')
+
+    if name and description and datetime:
+        meal = Meal(name=name, description=description, datetime=datetime, is_in_diet=is_in_diet)
+        db.session.add(meal)
+        db.session.commit()
+        return make_response(jsonify({'message': 'Meal created successfully'}))
+    return make_response(jsonify({'message': 'Missing parameters'}), 400)
 
 
 if __name__ == '__main__':
